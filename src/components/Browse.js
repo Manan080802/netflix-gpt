0aa5a84e5e2a7eac779aaaaf2e2565c1c96323d8
Manan080802 <manvaghasiya882@gmail.com>
2025-08-19 12:24:07 +0530

    get the movie details in now_playing api

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./navigation/Header";
+import { API_OPTION, API_URL } from "../constants/config";
 
 const Browse = () => {
+  const getMovieData = async () => {
+    const api_url = API_URL + "movie/now_playing";
+
+    const data = await fetch(api_url, API_OPTION);
+    const movieDetails = await data.json();
+  };
+  useEffect(() => {
+    getMovieData();
+  }, []);
+
   return (
     <div>
       <Header></Header>
